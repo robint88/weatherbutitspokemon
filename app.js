3384b316e51ddb3411f30bfc77d3bb816fb85013
Robin Turner <robinianturner@gmail.com>
2020-05-03 22:09:45 +0100

    ammend css

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ window.addEventListener('load',()=>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
             
-            const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`
+            
             fetch(api)
                 .then(response => {
                     return response.json();
@@ -20,12 +22,13 @@ window.addEventListener('load',()=>{
 
                     if(description.includes('cloud')){
                         document.querySelector('.background').style.backgroundImage = "url('https://i.pinimg.com/originals/5e/6a/9d/5e6a9d63809d1f2282cbe3df7aabad4e.jpg')";
-                    } else if(document.includes('rain')) {
+                    } else if(description.includes('rain')) {
                         document.querySelector('.background').style.backgroundImage = "url('https://cdn.bulbagarden.net/upload/3/37/Rain_IV_Field.png')";
                     } else if(description.includes('thunderstorm')){
                         document.querySelector('.background').style.backgroundImage = "url('https://vignette.wikia.nocookie.net/pokemon/images/0/0d/Spearow_anime.png/revision/latest?cb=20170530183524')";
                     } else if(description.includes('snow')){
                         document.querySelector('.background').style.backgroundImage = "url('https://cdn.bulbagarden.net/upload/2/2c/Hail_IV_Field_Light.png')";
+                        description = "stormy";
                     }  else if(description.includes('mist')) {
                         document.querySelector('.background').style.backgroundImage = "url('https://cdn.bulbagarden.net/upload/thumb/f/f1/Mist_VI.png/250px-Mist_VI.png')";
                     } else {
